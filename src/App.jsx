664f6d5cfff9c068d8240edff50dbaf5df0c2024
Carlos Beltran <carlossbel09@gmail.com>
2024-10-19 01:07:24 -0600

    perfil completo

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import MainPage from './views/mainPage.jsx';
 import Auth from './views/auth/Auth.jsx';
 import Courses from './views/courses/courses.jsx';
 import CourseDetail from './views/courses/course.jsx'; // Renombrado para evitar confusión
+import Profile from './views/account/Profile.jsx';
+import Pay from './views/account/Pay.jsx';
+import Mycourses from './views/account/Mycourses.jsx';
 
 function App() {
   return (
@@ -12,8 +15,11 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/auth" element={<Auth />} />
         <Route path="/courses" element={<Courses />} />
-        <Route path="/course/:name" element={<CourseDetail />} /> {/* Ruta dinámica con solo el nombre del curso */}
+        <Route path="/course/:name" element={<CourseDetail />} />
         <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/pay" element={<Pay />} />
+        <Route path="/mycourses" element={<Mycourses />} />
       </Routes>
     </Router>
   );
